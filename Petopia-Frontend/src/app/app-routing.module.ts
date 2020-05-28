8424571378d36a738371fb41bed5b7b2e70181f6
yisaleh <yara_ahmed_saleh@yahoo.com>
2020-05-28 18:04:09 +0200

    single service page

--- Petopia-Frontend/src/app/app-routing.module.ts
+++ Petopia-Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SingleComponent } from './components/single/single.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'home',
+  component: HomeComponent
+  },
+  {path: 'single',
+  component: SingleComponent} 
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  ],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
