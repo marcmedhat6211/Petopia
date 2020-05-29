65df39b32b1a2233e3d336a9734f2527efd00ba7
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-29 20:42:55 +0200

    add laravel-admin panel

---
+++ Petopia-Backend/app/Pet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    public function reservations()
+    {
+        return $this->hasMany('App\Reservation');
+    }
+
+    public function boardings()
+    {
+        return $this->hasMany('App\Boarding');
+    }
+
+    public function consultations()
+    {
+        return $this->hasMany('App\Consultation');
+    }
+}
