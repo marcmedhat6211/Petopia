ddbd6572ae64e21dedc7afe2a66aebee935683e7
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-06-01 14:09:50 +0200

    api authentication

--- Petopia-Backend/routes/api.php
+++ Petopia-Backend/routes/api.php
@@ -14,10 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-    /* REGISTRATION ROUTE */
+   
+/*REGISTER ROUTE*/
 
-Route::post('/register','Api\AuthController@register');
+Route::post('/register','RegisterController@register');
+
+/*LOGIN ROUTE */
+
+Route::post('/login','LoginController@login');
+
+/* LOGOUT ROUTE */
+
+Route::post('/logout','LoginController@logout');
