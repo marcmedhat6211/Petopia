a4dbc4d9c711293f7699f82dcf90ee57544ed21e
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-06-01 23:28:01 +0200

    retrieving data for the single page

--- Petopia-Frontend/src/app/components/home/home.component.ts
+++ Petopia-Frontend/src/app/components/home/home.component.ts
@@ -11,12 +11,22 @@ import { Service } from '../../Service';
 export class HomeComponent implements OnInit {
 
   service: Service;
+  // serviceId: Service;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void 
   {
     this.http.get<Service>('http://localhost:8000/api/services').subscribe(data => {
+      // console.log(data);
+      this.service = data;
+    });
+  }
+
+  onClick(serviceId)
+  {
+    console.log('ana gwa el fn');
+    this.http.get<Service>('http://localhost:8000/api/services/serviceId').subscribe(data => {
       console.log(data);
       this.service = data;
     });
